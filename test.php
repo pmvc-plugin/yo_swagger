6dd3f2388fb6b425c335e73ba4f2f9214765da31
Hill <hill@kimo.com>
2015-07-28 00:06:45 +0400

    move most swagger logic to swagger plugin

--- test.php
+++ test.php
@@ -1,25 +1,16 @@
 <?php
 PMVC\Load::plug();
 PMVC\addPlugInFolder('../');
-class HelloTest extends PHPUnit_Framework_TestCase
+class YoSwaggerTest extends PHPUnit_Framework_TestCase
 {
     function testPlugin()
     {
         ob_start();
-        $plug = 'hello_world';
+        $plug = 'yo_swagger';
         print_r(PMVC\plug($plug));
         $output = ob_get_contents();
         ob_end_clean();
         $this->assertContains($plug,$output);
     }
 
-    function testHello()
-    {
-        $willSay = 'hello, World!';
-        ob_start();
-        PMVC\plug('hello_world')->say($willSay);
-        $output = ob_get_contents();
-        ob_end_clean();
-        $this->assertContains($willSay,$output);
-    }
 }
--- yo_swagger.php
+++ yo_swagger.php
@@ -1,147 +1,70 @@
 <?php
 namespace PMVC\PlugIn\yo_swagger;
-
-// \PMVC\l(__DIR__.'/xxx.php');
+use PMVC\PlugIn\swagger;
 
 ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\yo_swagger';
 
-class yo_swagger extends \PMVC\PlugIn
+
+\PMVC\initPlugin(array('swagger'=>null));
+
+class yo_swagger extends swagger\swagger 
 {
-    public $swagger;
 
-    public function setInfo($title, $version='1.0')
+    public function init()
     {
-        $this->swagger['info'] = array(
-            'title'=>$title,
-            'version'=>$version
-        );
-        return $this;
-    }
-
-    public function setUrl($url)
-    {
-        $uri = parse_url($url);
-        $this->swagger['host']=$uri['host'];
-        $this->swagger['schemes']=array($uri['scheme']);
-        $this->swagger['basePath']=$uri['path'];
-        return $this;
-    }
-
-    /**
-     *    {
-     *        type: "apiKey",
-     *        name: "api_key",
-     *        in: "header"
-     *    }
-     */
-    public function setSecurity($key,$arr)
-    {
-        $this->swagger['securityDefinitions'][$key] = $arr;
-        return $this;
+        $this->swagger =\PMVC\plug('swagger')->get();
     }
 
     public function getSpec($yo)
     {
         $routes = $yo->getRoutes();
-        $arr = array(
-            'swagger'=>'2.0',
-            'consumes'=>array(
-                'application/json'
-            ),
-            'produces'=>array(
-                'application/json'
-            ),
-            'paths'=>array(),
-            'definitions'=>array()
-        );
-        $paths = \PMVC\plug('swagger')->get('paths');
         $annotation = \PMVC\plug('annotation');
+        $paths = $this->get('paths');
         foreach ($routes as $r) {
             $uri =$r['uri'];
             $method = strtolower($r['method']);
-            if (!isset($root['paths'][$uri])){ 
-                $arr['paths'][$uri] = array();
-            }
             $doc = $annotation->get($r['action']);
-            $arr['definitions'] = \PMVC\array_merge(
-                $arr['definitions'],
-                $this->parseDefinitions($doc)
-            );
-            $default = array(
-                'description'=>null
-                ,'parameters'=>null
-                ,'security'=> null 
-                ,'responses'=> array ( 
-                    '200' => array(
-                        'description'=>'success'
-                    )
-                )
-                ,'summary'=> null
-                ,'tags'=> null
-            );
-            $default = \PMVC\mergeDefault($default,$doc);
-            foreach ($default as $defk=>$defv) {
-                if (empty($default[$defk])) {
-                    unset($default[$defk]);
-                }
+            $this->parseDefinitions($doc);
+            if (!empty($doc['parameters'])) {
+                $doc['parameters'] =  $this->parseParameters($doc);
             }
-            if (!empty($default['parameters'])) {
-                $default['parameters'] = $doc->parseDataTypes($default['parameters'],'example');
-            }
-            if (!empty($default['security'])) {
-                $default['security'] = \PMVC\toArray($default['security']);
-            }
-            $arr['paths'][$uri][$method] = $default;
+            $paths[$uri][$method]->mergeDefault($doc);
         }
-        if (empty($arr['definitions'])) {
-            unset($arr['definitions']);
-        } else {
-            $arr = $this->parseParametersType($arr);
-        }
-        $arr = \PMVC\array_merge($arr,$this->swagger);
-        return $arr;
+        return $this->gen();
     }
 
-    function parseParametersType($arr)
+    function parseParameters($doc)
     {
-        foreach ($arr['paths'] as &$methods) {
-            foreach ($methods as &$method) {
-                if (empty($method['parameters'])) {
-                    continue;
-                }
-                foreach ($method['parameters'] as &$param) {
-                    if (isset($arr['definitions'][$param['type']])) {
-                        $param['schema'] = array(
-                            '$ref'=>'#/definitions/'.$param['type']
-                        );
-                        unset($param['type']);
-                    }
-                }
-            }
+        $doc['parameters'] = $doc->parseDataTypes($doc['parameters'],'example');
+        $parameters = new swagger\parameters();
+        foreach($doc['parameters'] as $param){
+            $parameter = new swagger\parameter();
+            $parameter->mergeDefault($param);
+            $parameters[] = $parameter; 
         }
-        return $arr;
+        return $parameters;
     }
-    
+
     function parseDefinitions($doc)
     {
-        $arr = array();
         if (empty($doc)) {
             return;
         }
         foreach ($doc as $k=>$v) {
             if(0 === strpos($k,'definitions')){
                 $keys = explode('-',$k);
-                $arr[$keys[1]] = $this->getDefinitionsKeys(
+                $this->getDefinitionsKeys(
                     $keys[1]
                     ,$doc
                 );
             }
         }
-        return $arr;
     }
 
     function getDefinitionsKeys($groupid,$doc){
-        $keys = array ('required','properties');
+        $definitions = $this->get('definitions');
+        $definition =  $definitions[$groupid];
+        $keys = array_keys($definition->getDefault());
         $default = array();
         foreach($keys as $k){
             $default[$groupid.'-'.$k] = '';
@@ -161,6 +84,7 @@ class yo_swagger extends \PMVC\PlugIn
            $properties_arr[$name] = $v; 
         }
         $arr['properties'] = $properties_arr;
+        $definition->mergeDefault($arr);
         return $arr;
     }
 }
